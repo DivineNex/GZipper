7795c706b09457b94c29e317daf7ebc8a0398db3
MizuriShine <nexterrible@mail.ru>
2024-07-29 15:03:45 +0700

    feat: minimal functionality

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var inputString = Console.ReadLine();
+
+            ///////////TEST
+            //var dateTimeTicks = DateTime.Now.Ticks;
+
+            //Zipper.Compress("C:\\Users\\nexte\\Desktop\\28.mp3", $"C:\\Users\\nexte\\Desktop\\COMPRESSED{dateTimeTicks}.gz");
+            //Zipper.Decompress($"C:\\Users\\nexte\\Desktop\\COMPRESSED{dateTimeTicks}.mp3.gz", "C:\\Users\\nexte\\Desktop\\DECOMPESSED.mp3");
+            ///////////TEST
+
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                var splittedInput = inputString.Split(' ').ToList();
+
+                if (splittedInput.Count > 0)
+                {
+                    if (splittedInput[0].ToLower() == "compress")
+                        Zipper.Compress(splittedInput[1], splittedInput[2]);
+                    else if (splittedInput[0].ToLower() == "decompress")
+                        Zipper.Decompress(splittedInput[1], splittedInput[2]);
+                }
+            }
         }
     }
 }
---
+++ Zipper.cs
@@ -0,0 +1,41 @@
+﻿using System.IO.Compression;
+
+namespace GZipper
+{
+    internal static class Zipper
+    {
+        internal static void Compress(string inputPath, string outputPath)
+        {
+            var splittedInput = inputPath.Split('.');
+            var splittedOutput = outputPath.Split('.');
+            splittedOutput[^1] = $".{splittedInput[^1]}.{splittedOutput[^1]}";
+
+            outputPath = string.Concat(splittedOutput);
+
+            using FileStream inputFileStream = File.Open(inputPath, FileMode.Open);
+            using FileStream compressedFileStream = File.Create(outputPath);
+            using var compressor = new GZipStream(compressedFileStream, CompressionMode.Compress);
+
+            inputFileStream.CopyTo(compressor);
+        }
+
+        internal static void CompressAsync(string inputPath, string outputPath)
+        {
+
+        }
+
+        internal static void Decompress(string inputPath, string outputPath)
+        {
+            using FileStream inputFileStream = File.Open(inputPath, FileMode.Open);
+            using FileStream decompressedFileStream = File.Create(outputPath);
+            using var decompressor = new GZipStream(inputFileStream, CompressionMode.Decompress);
+
+            decompressor.CopyTo(decompressedFileStream);
+        }
+
+        internal static void DecompressAsync(string inputPath, string outputPath)
+        {
+
+        }
+    }
+}
